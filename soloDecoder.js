1a31d354f4a6e578a0dcd4ed02060b4ff5882a55
Cash Mcgriddy <99050972+CashBowen@users.noreply.github.com>
2026-03-30 20:38:01 -0600

    Implement decode function in soloDecoder.js

---
+++ soloDecoder.js
@@ -0,0 +1,9 @@
+function decode(input) {
+	let encoded = input.split("$$").find(p => p.startsWith("pull:")).split(":")[1]
+	let encodedSplit = encoded.split(",")
+    return encodedSplit.map((q, i) => {
+        let parsed = parseInt(q.substr(q.length - 3, 3), 10);
+        let value = parsed - 13 - i;
+        return String.fromCharCode(value);
+    });
+}
