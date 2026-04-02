ba12accc4b73450a4073d4b6cc045d9f8d7719a7
etrnalembers <122410731+etrnalembers@users.noreply.github.com>
2026-04-02 09:29:44 -0600

    encoder split hotfix
    Whoops! This time, the function did not execute because the split encoder functions didn't have an array input from the assessment answer cookies. Hopefully the dead end is fixed.

--- soloDecoder.js
+++ soloDecoder.js
@@ -3,7 +3,7 @@ let cookie = frame.Nida.Cookies.get("assessment");
 
 function decode(input) {
     let encoded = input.split("$$").find(p => p.startsWith("pull:")).split(":");
-    let encodedSplit = encoded.split(",");
+    let encodedSplit = encoded[1].split(",");
     return encodedSplit.map((q, i) => {
         let parsed = parseInt(q.substr(q.length - 3, 3), 10);
         let value = parsed - 13 - i;
